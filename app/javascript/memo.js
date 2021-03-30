e2c7855a410a4926c05fda22313ea95826c86309
hatano <45449062+hatanotakahiro@users.noreply.github.com>
2021-03-30 17:00:05 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,10 +1,7 @@
-function post() {
+function post (){
+  const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-  // htmlの記述からidを取得する
-  const submit = document.getElementById("submit");
-  // クリックしたら発火するようにする
-  submit.addEventListener("click", () => {
     const form = document.getElementById("form");
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
